a4bac24ad524c3f40151a8aaf097dc68be65c38c
Marek Kubiczek <marek.kubiczek@imgtec.com>
2017-02-02 13:45:27 +0100

    update modification copyrights
    Signed-off-by: Marek Kubiczek <marek.kubiczek@imgtec.com>

--- advertise-url.py
+++ advertise-url.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # Copyright 2015 Opera Software ASA. All rights reserved.
-# Modifications copyright (C) 2017 Imagination Technologies
+# Modifications copyright (C) 2017 Imagination Technologies Limited and/or its affiliated group companies.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
